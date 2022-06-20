047da6f933be0e462daf718527cf962acaeaecb5
mkulesho <maxim.kuleshov@gmail.com>
2022-06-21 00:58:00 +0300

    Removed dontMock from test

--- 10-routes-browser-history-api/1-dashboard-page/index.spec.js
+++ 10-routes-browser-history-api/1-dashboard-page/index.spec.js
@@ -4,8 +4,6 @@ describe('routes-browser-history-api/dashboard-page', () => {
   let dashboardPage;
 
   beforeEach(async () => {
-    fetchMock.dontMock();
-    
     dashboardPage = new DashboardPage();
     const element = await dashboardPage.render();
 
